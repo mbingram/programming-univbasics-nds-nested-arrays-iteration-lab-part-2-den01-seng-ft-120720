d0bc372496a705dd96dc9b293b080c805440815c
charming-input-2976 <mbingramm@gmail.com>
2020-11-27 23:43:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,8 @@
 def find_min_in_nested_arrays(src)
-  
+  row_index = 0
+  while row_index < array.count do
+    element_index = 0
+    minimun = " "
+    while element_index < array[row_index].count do
+      
 end
